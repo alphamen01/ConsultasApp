812426f1d1b21e5864e77dbc0192a2e2ad0718bb
alphamen01 <lesg.2018@hotmail.com>
2023-07-07 13:23:01 -0500

    REPORTE FUNCIONAL OK

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -1,3 +1,4 @@
+import io.cucumber.java.AfterAll;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
@@ -8,6 +9,19 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("/features")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, json:target/cucumber.json")
 public class RunCucumberTest {
+    @AfterAll
+    public static void finish(){
+        try {
+            System.out.println("Generando reporte");
+            String[] cmd = {"cmd.exe", "/c", "npm run report"};
+            Runtime.getRuntime().exec(cmd);
+            System.out.println("Reporte Generado Satisfactoriamente!");
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
 }
